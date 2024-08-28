658124fa8696a5a19f1ce16c2054a522817b1ad0
hucu314 <hcumpean@ftstudent.org>
2024-08-28 18:35:49 -0400

    Better Camera
    Fixed the camera, it now follows both players. You can edit specific values inside script.

--- scrips/Camera2D.gd
+++ scrips/Camera2D.gd
@@ -1,15 +1,48 @@
 extends Camera2D
 
-@onready var player1 = %player1
-@onready var player2 = %player2
+@export var move_speed = 0.5
+@export var zoom_speed = 0.05
+@export var min_zoom = 2.5
+@export var max_zoom = 5
+@export var margin = Vector2(400,200)
+
+var targets = []
+
+@onready var screen_size = get_viewport_rect().size
+
+func add_target(t):
+	if not t in targets:
+		targets.append(t)
+		print(targets)
+		
+func remove_target(t):
+	if t in targets:
+		targets.erase(t)
+		print(targets)
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	var width = abs(player1.global_position.x-player2.global_position.x)+40
-	print(width)
+	if !targets:
+		return
+	var p = Vector2.ZERO
+	for target in targets:
+		p += target.position
+	p /= targets.size()
+	position = lerp(position,p,move_speed)
+	
+	var r = Rect2(position,Vector2.ONE)
+	for target in targets:
+		r = r.expand(target.position)
+	r = r.grow_individual(margin.x,margin.y,margin.x,margin.y)
+	var d = max(r.size.x,r.size.y)
+	var z
+	if r.size.x > r.size.y * screen_size.aspect():
+		z = clamp(r.size.x / screen_size.y,min_zoom,max_zoom)
+		zoom = lerp(zoom, Vector2.ONE * z, zoom_speed)
+		print(zoom)
 	pass
