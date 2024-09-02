eadc95d1860886ae85b4b29b2e3f415599c5a613
Zane <zagrismore@gmail.com>
2024-09-01 23:57:09 -0400

    Made  Buttons Interactable

--- Scripts/level_selector.gd
+++ Scripts/level_selector.gd
@@ -1,21 +1,27 @@
 extends Node
+@onready var animation_player: AnimationPlayer = $"../AnimationPlayer"
 
 
 func _on_level_1_button_pressed() -> void:
-	pass # Replace with function body.
-
+	#Stops music from playing twice?? Weird bug
+	Music.stop()
+	get_tree().change_scene_to_file("res://Scenes/Level1.tscn")
 
 func _on_level_2_button_pressed() -> void:
-	pass # Replace with function body.
-
+	#Stops music from playing twice?? Weird bug
+	Music.stop()
+	get_tree().change_scene_to_file("res://Scenes/Level1.tscn")
 
 func _on_level_3_button_pressed() -> void:
-	pass # Replace with function body.
-
+	#Stops music from playing twice?? Weird bug
+	Music.stop()
+	get_tree().change_scene_to_file("res://Scenes/Level1.tscn")
 
 func _on_level_4_button_pressed() -> void:
-	pass # Replace with function body.
-
+	#Stops music from playing twice?? Weird bug
+	Music.stop()
+	get_tree().change_scene_to_file("res://Scenes/Level1.tscn")
 
 func _on_back_button_pressed() -> void:
+	animation_player.play("LevelSelectorHide")
 	pass # Replace with function body.
--- Scripts/play_button.gd
+++ Scripts/play_button.gd
@@ -12,7 +12,5 @@ func _on_mouse_exited() -> void:
 
 
 func _on_pressed() -> void:
-	#Stops music from playing twice?? Weird bug
-	Music.stop()
-	get_tree().change_scene_to_file("res://Scenes/Level1.tscn")
+	animation_player.play("LevelSelectorShow")
 	pass # Replace with function body.
