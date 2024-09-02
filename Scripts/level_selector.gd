aa0669e23e979baaf987eca9508cdb0514dcb3a0
Zane <zagrismore@gmail.com>
2024-09-02 00:03:33 -0400

    Added Placeholder Levels

--- Scripts/level_selector.gd
+++ Scripts/level_selector.gd
@@ -10,17 +10,17 @@ func _on_level_1_button_pressed() -> void:
 func _on_level_2_button_pressed() -> void:
 	#Stops music from playing twice?? Weird bug
 	Music.stop()
-	get_tree().change_scene_to_file("res://Scenes/Level1.tscn")
+	get_tree().change_scene_to_file("res://Scenes/Level2.tscn")
 
 func _on_level_3_button_pressed() -> void:
 	#Stops music from playing twice?? Weird bug
 	Music.stop()
-	get_tree().change_scene_to_file("res://Scenes/Level1.tscn")
+	get_tree().change_scene_to_file("res://Scenes/Level3.tscn")
 
 func _on_level_4_button_pressed() -> void:
 	#Stops music from playing twice?? Weird bug
 	Music.stop()
-	get_tree().change_scene_to_file("res://Scenes/Level1.tscn")
+	get_tree().change_scene_to_file("res://Scenes/Level4.tscn")
 
 func _on_back_button_pressed() -> void:
 	animation_player.play("LevelSelectorHide")
