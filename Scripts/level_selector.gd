64be0b6f654e8f838bc7f0546f62ff6fbe2a4fc7
Zane <zagrismore@gmail.com>
2024-09-01 23:53:07 -0400

    Made Level Select Screen Appear

---
+++ Scripts/level_selector.gd
@@ -0,0 +1,21 @@
+extends Node
+
+
+func _on_level_1_button_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_level_2_button_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_level_3_button_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_level_4_button_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_back_button_pressed() -> void:
+	pass # Replace with function body.
