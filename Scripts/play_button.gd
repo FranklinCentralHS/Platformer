6c518022d3b8a85f30d1a93f83604f6e7d682338
Zane <zagrismore@gmail.com>
2024-09-01 23:35:45 -0400

    Added Level Select Screen

--- Scripts/play_button.gd
+++ Scripts/play_button.gd
@@ -1,14 +1,18 @@
 extends Button
+@onready var animation_player: AnimationPlayer = $"../../AnimationPlayer"
 
 func _on_mouse_entered() -> void:
 	#Scales button up when it's hovered over
-		scale=Vector2(1.05,1.05) 
+	animation_player.play("ScaleUp")
+		 
 
 func _on_mouse_exited() -> void:
 	#Resets the button once it's not hovered over
-	scale=Vector2(1,1)
+	animation_player.play("ScaleDown")
 
 
 func _on_pressed() -> void:
+	#Stops music from playing twice?? Weird bug
+	Music.stop()
 	get_tree().change_scene_to_file("res://Scenes/Level1.tscn")
 	pass # Replace with function body.
