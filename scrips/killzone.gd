b2b39d3a229c2f93a080edf55fa44ff56f003e11
hucu314 <hcumpean@ftstudent.org>
2024-09-03 14:08:22 -0400

    Life Counter
    counts lives and deletes when attacked

--- scrips/killzone.gd
+++ scrips/killzone.gd
@@ -4,14 +4,22 @@ extends Area2D
 @onready var mult_target_cam = %MultTargetCam
 @onready var player1 = %player1
 @onready var player2 = %player2
+@onready var lives = 3
+@onready var livesLabel = %Lives
 
 
 func _on_body_entered(body):
-	print("You died")
-	Engine.time_scale = 0.5
-	body.get_node("CollisionShape2D").queue_free()	
-	timer.start()
-	pass # Replace with function body.
+	print("You got damaged")
+	lives = lives-1
+	print(lives)
+	livesLabel.text = "Lives: "
+	if lives == 0:
+		print("You died")
+		print(lives)
+		Engine.time_scale = 0.5
+		body.get_node("CollisionShape2D").queue_free()	
+		timer.start()
+		pass # Replace with function body.
 
 
 func _on_timer_timeout():
