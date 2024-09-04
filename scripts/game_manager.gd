cd229748c2f74696f7a3f9a4db42a2b1b164caf7
owilling <owilling@ftstudent.org>
2024-09-04 13:57:27 -0400

    changes

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -3,9 +3,7 @@ extends Node
 var score = 0
 
 @onready var score_label = $ScoreLabel
-@onready var hud_coins = $HUD/Coins
 
 func add_point():
 	score+=1
 	score_label.text = "You collected " + str(score) + " coins"
-	hud_coins.text = str(score)
