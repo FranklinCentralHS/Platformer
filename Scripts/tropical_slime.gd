34d8f9733dac419b4e46c729d4eb2ede62e0d825
Devin Welty <dwelty@ftstudent.org>
2024-09-10 13:03:39 -0400

    Fixed Attack and Add Coins

--- Scripts/tropical_slime.gd
+++ Scripts/tropical_slime.gd
@@ -2,7 +2,7 @@ extends Node2D
 
 const speed = 60
 var direction =1
-
+@onready var kill_zone = $killZone
 @onready var ray_cast_right = $RayCastRight
 @onready var ray_cast_left = $RayCastLeft
 @onready var ray_cast_leftB = $RayCastLeftB
@@ -29,7 +29,8 @@ func _process(delta):
 
 func _on_slime_hurtbox_body_entered(body:Node2D):
 	print("i'm triggered")
-	print(body.get_class())
 	if body is Player:
+		kill_zone.queue_free()
 		queue_free()
-		print("DEATH")
+		
+		
