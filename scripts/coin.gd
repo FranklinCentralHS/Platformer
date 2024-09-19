c3747a23a94683c6cc19461947cdc8cf11570c6e
LSoots <Logan.soots@gmail.com>
2024-09-19 14:00:23 -0400

    Add-ons

--- scripts/coin.gd
+++ scripts/coin.gd
@@ -3,6 +3,6 @@ extends Area2D
 @onready var game_manaager = %GameManager
 @onready var animation_player = $AnimationPlayer
 
-func _on_body_entered(body:Node2D):
+func _on_body_entered(_body:Node2D):
 	game_manaager.add_point()
 	animation_player.play("pickup")	
